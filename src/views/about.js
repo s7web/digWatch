910389c68c2cdca4a0f11cdb67020075a5d4c6cf
dkikas <dkikas@s7designcreative.com>
2019-11-15 11:53:44 +0100

    Email in About page

--- src/views/about.js
+++ src/views/about.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, ScrollView, Dimensions, Text } from 'react-native';
+import {
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  Text,
+  Button,
+  Linking,
+} from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
 import { ApiRequests } from '../api/requests';
 
@@ -9,13 +16,25 @@ const SCREEN_HEIGHT = Dimensions.get('window').height;
 export default AboutPage = ({ navigation }) => {
   const [content, setContent] = useState(false);
   useEffect(() => {
-    console.log('navigation', navigation);
     if (navigation.state.params == undefined) {
       navigation.push('About', { once: true });
     }
 
     ApiRequests.aboutPageContent().then(content => {
-      setContent(content['data']['rows'][0]);
+      let data = content['data']['rows'][0];
+
+      data.email = data.body.match(
+        /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g
+      );
+
+      data.body = data.body.replace(data.email.toString(), '-split-');
+      data.body = data.body.split('-split-');
+      data.body = data.body[0];
+      data.email = data.email.toString();
+
+      console.log('email', data);
+
+      setContent(data);
     });
   }, []);
   return (
@@ -24,6 +43,10 @@ export default AboutPage = ({ navigation }) => {
         <>
           <Text style={styles.title}>{content.title}</Text>
           <Text style={styles.body}>{content.body}</Text>
+          <Button
+            onPress={() => Linking.openURL(`mailto:${content.email}`)}
+            title={content.email}
+          />
         </>
       )}
     </ScrollView>
