1ecee400c3963047b27270c54707d4254050dac0
Milan Hinic <milan.hinic@s7designceative.com>
2020-12-18 13:51:59 +0100

    Changed to new route

--- mobile/src/api/requests.js
+++ mobile/src/api/requests.js
@@ -3,32 +3,32 @@ import axios from 'axios';
 export const baseUrl = 'https://dig.watch';
 
 export const apiRoutes = {
-  conferences: '/appconferences?page=',
-  conferencesDay: '/appsessions4conferencegroupedbyday/',
+  conferences: '/jit-conferences.json?page=',
+  conferencesDay: '/jit-conferences-by-day.json/',
   conferencesDayReport: '/jsonapi/node/conference_sessions/',
-  conferencesSessionDayReport: '/appconferencesessiondetails/',
-  conferencesSearch: '/appconferencesessions/',
-  aboutPageContent: '/appaboutjit',
-  conferenceIssues: '/appissues4conference/',
+  conferencesSessionDayReport: '/jit-report.json/',
+  conferencesSearch: '/jit-search.json/',
+  aboutPageContent: '/jit-about.json',
+  conferenceIssues: '/jit-issues.json',
 };
 
 export const ApiRequests = {
-  conferences: route => {
+  conferences: (route) => {
     return axios.get(baseUrl + route);
   },
-  conferencesDay: route => {
+  conferencesDay: (route) => {
     return axios.get(baseUrl + route);
   },
-  conferencesDayReport: route => {
+  conferencesDayReport: (route) => {
     return axios.get(baseUrl + route);
   },
-  conferencesSearch: route => {
+  conferencesSearch: (route) => {
     return axios.get(baseUrl + apiRoutes.conferencesSearch + route);
   },
-  conferenceIssues: confId => {
+  conferenceIssues: (confId) => {
     return axios.get(baseUrl + apiRoutes.conferenceIssues + confId);
   },
-  saveToken: route => {
+  saveToken: (route) => {
     return axios.get('http://78.47.162.187/store-token/' + route);
   },
   aboutPageContent: () => {
